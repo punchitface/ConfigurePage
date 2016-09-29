b4c515185d8a097b762420f42aaaab193265e417
punchitface <punchitface@gmail.com>
2016-09-29 16:37:05 +0300

    CreateForm
    asdasd

--- scripts/config.js
+++ scripts/config.js
@@ -2,12 +2,14 @@
     var add_tel = $(".add-contact-tel")[0];
     var tel_context = $(".block-two")[0];
     var add_adr = $(".add-contact-adr");
-    var previous = $();
 
     add_tel.onclick = function() {
         var tel_input = document.createElement("input");
         tel_input.className = "input user_phone";
+        tel_input.style.position = "relative";
         tel_input.placeholder = "Доп. телефон:";
         tel_context.insertBefore(tel_input, add_tel);
-    }
+    };
+
+ 
 }());
